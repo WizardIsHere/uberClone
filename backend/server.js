9be7aba1e98647dce6223f794e120a07640275f0
WizardIsHere <mshushant235@gmail.com>
2025-03-01 09:47:58 +0530

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const http= require('http');
+const app = require('./app');
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+
+// Handle unhandled promise rejections
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
