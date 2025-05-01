a6646082aaac9ad60624b952170d903552898f4f
wizardIsHere <mshushant235@gmail.com>
2025-05-01 16:16:29 +0530

    User and Captain SignIn with Context setUp

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 
 const Home = () => {
     return (
-        <div className='h-screen bg-cover bg-bottom bg-[url(https://images.unsplash.com/photo-1602773040364-522556233e30?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHRyYWZmaWMlMjBsaWdodHxlbnwwfHwwfHx8MA%3D%3D)] pt-8 flex justify-between flex-col w-full bg-red-400'>
+        <div className='h-screen bg-cover bg-bottom bg-[url(https://images.unsplash.com/photo-1602773040364-522556233e30?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHRyYWZmaWMlMjBsaWdodHxlbnwwfHwwfHx8MA%3D%3D)] pt-8 flex justify-between flex-col w-full '>
             <img className="w-16 ml-8" src="" />
             <div className='bg-white pb-7 px-4 py-4'>
                 <h2 className='text-3xl font-bold'>Get started with Uber</h2>
--- frontend/src/pages/UserSignup.jsx
+++ frontend/src/pages/UserSignup.jsx
@@ -1,9 +1,104 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
 
 const UserSignup = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [userData, setUserData] = useState({})
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+
+        setUserData({
+            email: email,
+            password: password,
+            fullName:{
+                firstName:firstName,
+                lastName:lastName,
+            },
+        })
+        console.log("User Data: ", userData)
+        setEmail('');
+        setPassword('');
+        setFirstName('');
+        setLastName('');
+    }
+
     return (
-        <div>
-            
+        <div className="p-7 flex flex-col h-screen justify-between items-center bg-white rounded-lg">
+            <div>
+                <img
+                    className="w-16 mb-10"
+                    src="https://download.logo.wine/logo/Uber/Uber-Logo.wine.png"
+                    alt="Uber Logo"
+                />
+                <form onSubmit={(e)=>{
+                    submitHandler(e)
+                }}>
+                    <h3 className="text-lg font-medium mb-2">What's your name?</h3>
+                    <div className="flex gap-4 mb-5" >
+                        <input
+                            className="bg-[#eeeeee] rounded w-1/2 px-4 py-2 border text-lg placeholder:text-base"
+                            required
+                            value={firstName}
+                            onChange={(e)=>{
+                                setFirstName(e.target.value)
+                            }}
+                            placeholder="First Name"
+                        />
+                        <input
+                            className="bg-[#eeeeee] rounded w-1/2 px-4 py-2  border text-lg placeholder:text-base"
+                            required
+                            value={lastName}
+                            onChange={(e)=>{
+                                setLastName(e.target.value)
+                            }}
+                            type="text"
+                            placeholder="Last Name"
+                        />
+                    </div>
+
+                    <h3 className="text-lg font-medium mb-2">What's your email?</h3>
+                    <input
+                        className="bg-[#eeeeee] mb-5 rounded px-4 py-2 w-full  border text-lg placeholder:text-base"
+                        required
+                        type="email"
+                        value={email}
+                        onChange={(e)=>{
+                            setEmail(e.target.value)
+                        }}
+                        placeholder="email@example.com"
+                    />
+
+                    <h3 className="text-lg font-medium mb-2">Enter Password</h3>
+                    <input
+                        className="bg-[#eeeeee] mb-5 rounded px-4 py-2 w-full border text-lg placeholder:text-base"
+                        required
+                        type="password"
+                        onChange={(e)=>{
+                            setPassword(e.target.value)
+                        }}
+                        placeholder="Password"
+                    />
+
+                    <button
+                        className="bg-[#111] text-white font-semibold mb-3 rounded px-4 py-2 w-full text-lg"
+                        type="submit"
+                    >
+                        Sign In
+                    </button>
+                    <p className='text-center'>Already have an account? <Link className='mb-3 text-blue-600' to="/login">Login here</Link></p>
+                </form>
+            </div>
+
+            <div>
+                <p>
+                    <p className='text-[10px] leading-tight'>This site is protected by reCAPTCHA and the <span className='underline'>Google Privacy
+            Policy</span> and <span className='underline'>Terms of Service apply</span>.</p>
+                </p>
+            </div>
         </div>
     );
 };
